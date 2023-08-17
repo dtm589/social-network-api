2fd1c5c705b0632485e61b965d48c7c1d78cc697
Derek Marchese <derekmarchese1@gmail.com>
2023-08-17 12:51:25 -0400

    models

--- models/Thought.js
+++ models/Thought.js
@@ -1,10 +1,46 @@
 const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
+const dateFormat = require("../utils/dateFormat");
+
+const reactionSchema = new Schema(
+    {
+      reactionId: {
+        // Mongoose's ObjectId data type
+        type: Schema.Types.ObjectId,
+        // Default value is set to a new ObjectId
+        default: () => new Types.ObjectId(),
+      },
+  
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+  
+      username: {
+        type: String,
+        required: true,
+      },
+  
+      createdAt: {
+        type: Date,
+        // Set default value to the current timestamp
+        default: Date.now,
+        // Use a getter method to format the timestamp on query
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
 
 const thoughtSchema = new Schema(
     {
         thoughtText: { type: String, required: true, min: 1, max: 280 },
-        createdAt: { tpye: Date, default: new Date(), get: format },
+        createdAt: { tpye: Date, default: Date.now, get: (timestamp) => dateFormat(timestamp) },
         username: { type: String, required: true },
         reactions: [reactionSchema]
     },
@@ -14,16 +50,12 @@ const thoughtSchema = new Schema(
     }
 );
 
-function format() {                  //NEED TO FINISH THIS
-    return .toDateString();
-};
-
 thoughtSchema
     .virtual('reactionCount')
     .get(function () {
         return this.reactions.length;
     });
 
-const Thought = model('thought', thoughtSchema);
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
